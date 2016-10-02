7193fba4359ff59e194884dca15a0576756ae8ba
Sergey <sergeybirich@gmail.com>
2016-10-02 16:35:06 +0300

    Implemented JsonParser

---
+++ DtoGenerator/DtoGenerator/ClassField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DtoGenerator
+{
+    [DataContract]
+    class ClassField
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Type { get; set; }
+        [DataMember]
+        public string Format { get; set; }
+
+    }
+}
---
+++ DtoGenerator/DtoGenerator/JsonParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace DtoGenerator
+{
+    class JsonParser
+    {
+        private string sourceName;
+        public JsonParser (string filename)
+        {
+            sourceName = filename;
+        }
+        public ClassType[] GetClassesFromJson()
+        {
+            ClassType[] classTypes;
+            using (FileStream fstream = File.OpenRead(sourceName))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(ClassType[]));
+                classTypes = (ClassType[])serializer.ReadObject(fstream);
+            }
+            return classTypes;
+        }
+
+    }
+}
