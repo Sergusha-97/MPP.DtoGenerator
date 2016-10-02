a5d86bc419049420410a55f952bc2a5009e27c5a
Sergey <sergeybirich@gmail.com>
2016-10-02 18:49:21 +0300

    Added plugin BasicTypes

---
+++ DtoGenerator/BasicTypesLib/BasicTypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITypeMap;
+
+namespace BasicTypesLib
+{
+    public class BasicTypes: ITypeMap.ITypeMap
+    {
+        private Dictionary<string,Type> typeMap;
+        public Dictionary<string, Type> TypeMap
+        {
+            public get 
+            {
+                return typeMap;
+            }
+        }
+        public BasicTypes ()
+        {
+            typeMap = new Dictionary<string, Type>();
+            typeMap["int32"] = typeof(System.Int32);
+            typeMap["int64"] = typeof(System.Int64);
+            typeMap["float"] = typeof(System.Single);
+            typeMap["double"] = typeof(System.Int32);
+            typeMap["byte"] = typeof(System.Byte);
+            typeMap[""] = typeof(System.Boolean);
+            typeMap["date"] = typeof(System.DateTime);
+            typeMap["string"] = typeof(System.String);
+        }
+    }
+}
