ecd46e3932755819c18f33a131b3d696e15f8995
Sergey <sergeybirich@gmail.com>
2016-10-02 18:10:53 +0300

    Added interface for plugins

---
+++ DtoGenerator/ITypeMap/ITypeMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITypeMap
+{
+    interface ITypeMap
+    {
+        public Dictionary<string, Type> TypeMap { public get; private set; }
+    }
+}
