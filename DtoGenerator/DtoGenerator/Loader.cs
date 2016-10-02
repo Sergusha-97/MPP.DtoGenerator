0bc164cf1c4e7b0db99c38638fe97be5cc1256a2
Sergey <sergeybirich@gmail.com>
2016-10-02 18:20:30 +0300

    Added abstract class Loader

---
+++ DtoGenerator/DtoGenerator/Loader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITypeMap;
+
+namespace DtoGenerator
+{
+    abstract class Loader
+    {
+        abstract public List<ITypeMap.ITypeMap> LoadPlugins(string path);
+    }
+}
